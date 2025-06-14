3f01b0fb2ad8e65d98f30d7e2d4e0d3e9d3b8858
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-15 00:08:15 +0200

    first commit

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Conectado ao MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+        console.log("O Serividor está rodando na porta 3000!");
+    }
+);
