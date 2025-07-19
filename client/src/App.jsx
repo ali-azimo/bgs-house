ae5075b1802fea5d0fda0abb01cbec072063212d
Ali Azimo <webdesign.aejl@gmail.com>
2025-07-19 22:21:27 +0200

    mais uma vez

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
+import SignIn from "./pages/SignIn";
 import Profile from './pages/Profile'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
@@ -22,7 +22,7 @@ export default function App() {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
-            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-in' element={<SignIn/>} />
             <Route path='/sign-up' element={<SignUp />} />
             <Route path='/search' element={<Search />} />
             <Route path='/team' element={<Team />} />
