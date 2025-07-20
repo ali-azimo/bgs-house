694674c803fba8dc601a8ad55aca216fe8b8af58
Ali Azimo <webdesign.aejl@gmail.com>
2025-07-20 19:42:08 +0200

    agora

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
@@ -18,7 +17,6 @@ export default function App() {
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/search' element={<Search/>}/>
