9fe8c3e9f2ab032bda9acbb975c2543d8e0209dc
Ali Azimo <webdesign.aejl@gmail.com>
2025-07-20 22:43:37 +0200

    Geral

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ app.use("/api/user", userRouter);
 app.use('/api/auth', cadRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get("*splat", (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
