11110d2679ed77d2ffd030c3a19f3c07faa1497e
Ali Azimo <webdesign.aejl@gmail.com>
2025-07-20 21:46:56 +0200

    teste

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
     }
     const fetchSaleListings = async () =>{
       try{
-        const res = await fetch(`/api/listing/get?type=sale&limit=4`);
+        const res = await fetch(`https://bgs-house-back.onrender.com/api/listing/get?type=sale&limit=4`);
         const data = await res.json();
         setSaleListings(data);
        }catch(error){
